23d891c2c4593f33fffc5862cf183f908438dcd3
mikeskinart5 <mikeskinart5@icloud.com>
2026-09-14 22:23:22 +0800

    new tee design

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -59,7 +59,7 @@ export const PRODUCTS: Product[] = [
     price: "USD 38",
     image:
       "https://placehold.co/600x600/0a0a0a/c8a25a?text=Limited+Tee&font=raleway",
-    paymentUrl: "https://https://www.etsy.com/hk-en/listing/4575035053/sailor-skull-helm-tee-sailors-tattoo-t?ref=listings_manager_grid",
+    paymentUrl: "https://www.etsy.com/hk-en/listing/4575035053/sailor-skull-helm-tee-sailors-tattoo-t?ref=listings_manager_grid",
   },
   {
     id: "merch-tee-black",
