37cffb6a5b3e4d97dcb0d5cc9c8d0c37681e44a2
mikeskinart5 <mikeskinart5@icloud.com>
2026-09-14 22:42:44 +0800

    Add product image

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -58,7 +58,7 @@ export const PRODUCTS: Product[] = [
       "A2 archival giclée prints of three signature tattoo designs, signed and numbered. Edition of 50.",
     price: "USD 38",
     image:
-      "/Users/mikeskinart/Desktop/sailors tattoo hk/skull-captain.png",
+      "/shop/skull-captain.png",
     paymentUrl: "https://www.etsy.com/hk-en/listing/4575035053/sailor-skull-helm-tee-sailors-tattoo-t?ref=listings_manager_grid",
   },
   {
