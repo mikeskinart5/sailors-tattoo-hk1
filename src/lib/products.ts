f05c4a322ef50b63ae3d92bfa2cf110bb23e1550
mikeskinart5 <mikeskinart5@icloud.com>
2026-09-17 20:00:01 +0800

    Add Gumroad links to shop

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -35,7 +35,7 @@ export const PRODUCTS: Product[] = [
     price: "HKD 180",
     image:
       "/shop/smart ink.png",
-    paymentUrl: "https://mikeskinart.gumroad.com/l/pxhzs?_gl=1*10chjqb*_ga*MTI0NDIzNTg2OS4xNzg3OTk3NjQx*_ga_6LJN6D94N6*czE3ODk2NDQxNTIkbzM2JGcxJHQxNzg5NjQ2MTUzJGo1MiRsMCRoMA..",
+    paymentUrl: "https://mikeskinart.gumroad.com/l/pxhzs?_gl=1*10chjqb*_ga*MTI0NDIzNTg2OS4xNzg3OTk3NjQx*_ga_6LJN6D94N6*czE3ODk2NDQxNTIkbzM2JGcxJHQxNzg5NjQ2MTUzJGo1MiRsMCRoMA",
   },
 {
   id: "merch-tee-black",
